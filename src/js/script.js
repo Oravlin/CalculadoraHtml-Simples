fca7a8f5f38a520eceaf20940f2983d3369f5f9d
Alvaro <alvaroandradesantos2009@gmail.com>
2026-01-19 17:52:23 -0300

    continuando js

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,51 @@
 const container = document.querySelector('#botoes-container');
 
-let displayText = '0';
-let resultDisplay = 0;
+let displayText = "0";
+let resultDisplay = "0";
 let calc = [0, 0];
+let numero;
 
 container.addEventListener('click', (event) => {
     if (event.target.classList.contains('btn-calc')) {
         const idClick = event.target.id;
-        
-        if (parseFloat(Array.from(idClick)[5]) >= 0.0)
-            addTxtDisplay(idClick)
+        if (idClick.length == 5)
+            numero = addTxtDisplay(idClick)
+        document.getElementById("display-text").textContent = numero.toString();
+
+        if (idClick === "btn-apaga") {
+            document.getElementById("display-text").textContent = "0";
+            displayText = "0";
+            resultDisplay = "0";
+            calc = [0, 0]
+        }
+        if (idClick === "btn-mais") {
+            if (calc[0] === 0) {
+                calc[0] = numero;
+                console.log(calc);
+                document.getElementById("display-text").textContent = "0";
+                displayText = "0";
+                resultDisplay = "0";
+            }
+            else{
+                calc[1] = numero;
+                calc[0] = calc[0] + calc[1];
+                calc[1] = 0;
+                console.log(calc);
+                document.getElementById("display-text").textContent = calc[0].toString();
+                displayText = "0";
+                resultDisplay = "0";
+            }
+        }
     }
 })
 
 function addTxtDisplay(num) {
     const n = Array.from(num);
-    if (displayText !== "0") {
-        displayText =+ n[5];
+    if (displayText == "0") {
+        displayText = n[4];
     }
     else
-        displayText = n[5];
-    resultDisplay = parseFloat(displayText);
-    console.log(displayText)
-    console.log(n)
+        displayText = + n[4];
+    resultDisplay = resultDisplay + displayText;
+    return parseFloat(resultDisplay);
 }
